f687d74bc725870c3c0a22503c4e8ab331cd81cb
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-07 19:45:52 +0100

    refactor experiment naive imputation

--- source/models/naive/mlflow_naive_imputation.py
+++ source/models/naive/mlflow_naive_imputation.py
@@ -3,30 +3,29 @@ from mlflow.pyfunc import PythonModel
 
 
 class SimpleAvgImputation(PythonModel):
+    " Simple average imputation model. "
+
     def __init__(self, variable_name, group_by):
+
+        assert isinstance(variable_name, str), "Input variable_name must be a string."
+        assert isinstance(group_by, str), "Input group_by must be a string."
+        
         self.variable_name = variable_name
         self.group_by = group_by
         self.avg_mean = None
     
     def fit(self, training_df):
-        try:
-            self.avg_mean = training_df.groupby(self.group_by)[self.variable_name].mean()
-        except Exception as e:
-            print(f"Error during fitting: {e}")
+        " Fit the model. "
+        self.avg_mean = training_df.groupby(self.group_by)[self.variable_name].mean()
 
     def predict(self, test_df):
-        try:
-            predictions = self.avg_mean[test_df[self.group_by]].values
-            return predictions
-        except Exception as e:
-            print(f"Error during prediction: {e}")
-            return None
+        " Predict using the model. "
+        predictions = self.avg_mean[test_df[self.group_by]].values
+        return predictions
 
     @staticmethod
     def compute_rmse(targets, predictions):
-        try:
-            rmse = np.sqrt(np.sum((predictions - targets) ** 2) / len(predictions))
-            return rmse
-        except Exception as e:
-            print(f"Error computing RMSE: {e}")
-            return None
+        " Compute the root mean squared error. "
+        rmse = np.sqrt(np.sum((predictions - targets) ** 2) / len(predictions))
+        return rmse
+
