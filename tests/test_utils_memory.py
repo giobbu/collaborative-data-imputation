0b9571f1330315927bf77d6a7e58ee7ff6d4f29b
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-13 17:12:52 +0100

    add docstrings

--- tests/test_utils_memory.py
+++ tests/test_utils_memory.py
@@ -35,12 +35,14 @@ def test_update_period2farm_and_farm2period_train_valid(sample_df):
 
 # Test case: invalid input (not a DataFrame)
 def test_update_period2farm_and_farm2period_train_invalid_input():
+    " Test if an assertion error is raised for an invalid input. "
     with pytest.raises(AssertionError, match="Input df must be a pandas DataFrame."):
         update_period2farm_and_farm2period_train([1, 2, 3])
 
 
 # Test case: missing columns in DataFrame
 def test_update_period2farm_and_farm2period_train_missing_columns(sample_df_missing_columns):
+    " Test if an assertion error is raised for a DataFrame missing required columns. "
     # Call the function with a DataFrame missing required columns
     with pytest.raises(AssertionError, match="DataFrame df must contain columns 'periodId', 'farmId', and 'power_z'."):
         update_period2farm_and_farm2period_train(sample_df_missing_columns)
