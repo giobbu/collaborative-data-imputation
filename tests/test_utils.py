8a1eb72d9a6397176f911b66b0432548839d52e2
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-13 16:48:52 +0100

    add pytest invalid extensions (csv and xlsx)

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import pytest
+import pandas as pd
+from source.utils.utils_general import read_excel_file, read_nordpool_csv
+
+def test_read_excel_file_invalid_extension():
+    " Test if an assertion error is raised for an invalid file extension. "
+    with pytest.raises(AssertionError, match="Input file must be an Excel file."):
+        read_excel_file('test_file.csv')
+
+def test_read_csv_file_invalid_extension():
+    " Test if an assertion error is raised for an invalid file extension. "
+    with pytest.raises(AssertionError, match="Input file must be a CSV file."):
+        read_nordpool_csv('test_file.xlsx')
+
